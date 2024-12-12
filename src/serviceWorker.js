018fc4918d8aefc8a87ba66a1bdc760c0c0d4d9b
Savastane <savastane@gmail.com>
2024-12-12 00:48:46 -0300

    add pwa Configuração

---
+++ src/serviceWorker.js
@@ -0,0 +1,42 @@
+// This is a basic service worker script that caches all files for offline use.
+
+const CACHE_NAME = 'redecity-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.tsx',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
